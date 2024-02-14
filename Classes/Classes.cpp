1ec6d159e4f77271e71940024dda1052d279b056
eikatkoffbmstu <147157589+eikatkoffbmstu@users.noreply.github.com>
2024-02-14 12:38:52 +0300

    Add project files.

---
+++ Classes/Classes.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class PersComp {
+private:
+    string name;
+    int hrz;
+public:
+    PersComp(const string& n, int h) : name(n), hrz(h) {}
+
+    string get_name() const {
+        return name;
+    }
+
+    int get_hrz() const {
+        return hrz;
+    }
+
+    void print() const {
+        cout << "Processor is " << name << " and its Hrz is " << hrz << endl;
+    }
+    bool operator<(const PersComp& pc) const {
+        return hrz > pc.hrz;
+    }
+};
+
+int main() {
+    vector<PersComp> comps = {
+      PersComp("Intel i3", 345),
+      PersComp("Ryzen", 550),
+      PersComp("Intel i7", 700)
+    };
+
+    sort(comps.begin(), comps.end());
+
+    for (const auto& pc : comps) {
+        pc.print();
+    }
+
+    return 0;
+}
